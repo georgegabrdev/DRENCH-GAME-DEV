2f30d198d13f41172413ea639912870c97aadb8e
georgegabrdev <georgegabr2014@gmail.com>
2026-06-27 16:34:02 -0300

    hide from tmp

--- main.lua
+++ main.lua
@@ -1,4 +1,4 @@
--- name: \\#00ffd5\\Drench Game Deluxe v1.2.3
+-- name: \\#00ffd5\\Drench Game Deluxe v1.2.3 [WIP]
 -- description: Squid Game in Mario 64!\n\nCommissioned by Drenchy\nInspired by Dani's \"Crab Game\"\n\nProgramming: EmilyEmmi\n\nMaps: biobak, EmilyEmmi, Woissil\n\nSoundtrack: murioz, Awesome Seal Guy (YT)\n\nVoice Acting:\nEspi as Toad\nSqueex as Mingle Callout\nTrashcam as Waluigi\n\nAds: Squeex's Community\n\nSpecial Thanks: Squishy
 -- category: gamemode
 -- incompatible: gamemode
